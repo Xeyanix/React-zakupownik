5ebb185870e321ec41f3059941cbccd64530ccf4
Xeyanix <jan.goralewski@o2.pl>
2023-06-20 01:25:09 +0200

    task 3

--- zakupownik2/src/components/ProductsFilters/ProductsFilters.js
+++ zakupownik2/src/components/ProductsFilters/ProductsFilters.js
@@ -5,8 +5,32 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { filterProducts, showOnlyFood } from "../../redux/productsSlice";
 
 function ProductsFilters() {
+  const [searchPhrase, setsearchPhrase] = useState("");
+  const [isOnlyFood, setisOnlyFood] = useState(false);
+  const FilterPhraseFromState = useSelector(
+    (state) => state.products.searchFilter
+  );
+  const dispatch = useDispatch();
+  const handleSearchPhraseChange = (event) => {
+    setsearchPhrase(event.target.value);
+    dispatch(filterProducts(event.target.value));
+  };
+  const handleOnlyFoodChecked = (event) => {
+    setisOnlyFood(event.target.checked);
+    if (event.target.checked) {
+      dispatch(showOnlyFood(true));
+      dispatch(filterProducts(FilterPhraseFromState));
+    } else {
+      dispatch(showOnlyFood(false));
+      dispatch(filterProducts(FilterPhraseFromState));
+    }
+  };
+
   return (
     <div className={styles.filtersHeaderWrapper}>
       <Typography variant="h4">Filtruj produkty: </Typography>
@@ -18,14 +42,16 @@ function ProductsFilters() {
                 margin="dense"
                 label="Nazwa"
                 variant="outlined"
-                // value=''
-                // onChange={}
+                value={searchPhrase}
+                onChange={handleSearchPhraseChange}
               />
             }
           />
           <FormControlLabel
             control={<Checkbox />}
             label="Tylko produkty spożywcze"
+            value={isOnlyFood}
+            onChange={handleOnlyFoodChecked}
           />
         </div>
       </FormGroup>
