0e5f11642eb6a89e60bac065ead3df13361a8913
Xeyanix <jan.goralewski@o2.pl>
2023-06-20 15:57:39 +0200

    task 3- Done

--- zakupownik2/API/common/consts/products.js
+++ zakupownik2/API/common/consts/products.js
@@ -53,5 +53,78 @@ const products = [
     category: "warzywa",
     isFood: true,
   },
+
+  {
+    id: "10",
+    name: "jabłko",
+    category: "Fruits",
+    isFood: true
+  },
+  {
+    id: "11",
+    name: "Chleb",
+    category: "Bakery",
+    isFood: true
+  },
+  {
+    id: "12",
+    name: "woda",
+    category: "Dairy",
+    isFood: true
+  },
+  {
+    id: "13",
+    name: "kurczak",
+    category: "Meat",
+    isFood: true
+  },
+  {
+    id: "14",
+    name: "Pomidor",
+    category: "Vegetables",
+    isFood: true
+  },
+
+  {
+    id: "15",
+    name: "pomarańcza",
+    category: "Fruits",
+    isFood: true
+  },
+  {
+    id: "16",
+    name: "ryba",
+    category: "Fish",
+    isFood: true
+  },
+  {
+    id: "17",
+    name: "jajka",
+    category: "Dairy",
+    isFood: true
+  },
+  {
+    id: "18",
+    name: "marchewka",
+    category: "Vegetables",
+    isFood: true
+  },
+  {
+    id: "19",
+    name: "brokuł",
+    category: "Vegetables",
+    isFood: true
+  },
+  {
+    id: "20",
+    name: "cytryna",
+    category: "Vegetables",
+    isFood: true
+  }
+
+
+
+
+
 ];
 module.exports = products;
